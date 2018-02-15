79e88c3b97279953e1a5fcf799d5d3e82d1a2d62
saDNAaaa <wanao2008@hotmail.com>
2018-02-16 00:00:30 +0800

    mygit

---
+++ my.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+class sadnn(object):
+	def __init__(self, name, age):
+		self.__name = name
+		self.__age = age
+	def getData(self, main):
+		if main == "name":
+			return __name
+		elif main == "age":
+			return __age
+		else:
+			raise ValueError("the main is by zero")
+	def setData(self, newname, newage):
+		newname = self.__name
+		newage = self.__age
+k = sadnn("", 0)
+f = str(input("Plase enter you name:"))
+fk = int(input("Plase enter you age:"))
+k.setData(f, fk)
+file = open("~/tmp.txt")
+file.read()
+file.write(k.getData("name")\nk.getData("age"))
+file.close()
